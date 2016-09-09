d474595a40b7ebafadded262f04c8a553b9e8ea3
Steen Clarke <cmclarkey@gmail.com>
2016-09-08 20:10:26 -0700

    Make filter to show all employees too young to work

--- app/assets/javascripts/suspicious_filter.js
+++ app/assets/javascripts/suspicious_filter.js
@@ -1,7 +1,23 @@
 angular.module("app").filter('suspicious', [function () {
   return function(employees) {
+    var dateToday = new Date();
+    var yyyy = dateToday.getFullYear();
+
+    var cutoffYyyy = yyyy-15; //Return everybody who is too young to legally work
+
+    var youngsters = [];
+
+    var cutoffDate = new Date(cutoffYyyy);
     if (employees) {
-      return [employees[6]];
+        for (i=0; i<employees.length; i++) {
+            var employeeDate = new Date(employees[i].birthdate);
+            var employeeYear = employeeDate.getFullYear();
+            if (employeeYear>cutoffDate) {
+                youngsters.push(employees[i]);
+            }
+        }
     }
+
+    return youngsters;
   }
 }]);
